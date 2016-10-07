8b42bfc2c982f3cae72d01c21aa33c77d57b4542
Adrian Hooper <adrian@euphoricpanda.co.uk>
2016-10-07 10:49:51 +0100

    Added relationship

--- lib/roly_poly/role_resource.rb
+++ lib/roly_poly/role_resource.rb
@@ -2,5 +2,12 @@ module RolyPoly
   module RoleResource
     extend ActiveSupport::Concern
 
+    included do
+      class_attribute :adapter
+
+      has_many "#{RolyPoly.user_class_name.underscore}_privileges".to_sym, as: :resource
+
+    end
+
   end
 end
